41323606c616c50cec784663ee9767b5f8995ef2
Anthony B. Coates <abcoates@gmail.com>
2012-12-06 22:31:19 +0000

    Now working with Mysql as the database.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,25 +2,28 @@
 
 # --- !Ups
 
-CREATE SEQUENCE user_id_seq;
+#CREATE SEQUENCE user_id_seq; # for in memory DB
 CREATE TABLE user (
-    id integer NOT NULL DEFAULT nextval('user_id_seq'),
+#    id integer NOT NULL DEFAULT nextval('user_id_seq'), # for in memory DB
+    id integer primary key auto_increment,
     username varchar(255),
     password varchar(255),
     email varchar(255)
 );
 
-CREATE SEQUENCE symptom_id_seq;
+#CREATE SEQUENCE symptom_id_seq; # for in memory DB
 CREATE TABLE symptom (
-    id integer NOT NULL DEFAULT nextval('symptom_id_seq'),
+#    id integer NOT NULL DEFAULT nextval('symptom_id_seq'), # for in memory DB
+    id integer primary key auto_increment,
     userid integer NOT NULL,
     whichsymptom varchar(255),
     whensymptom varchar(255)
 );
 
-CREATE SEQUENCE event_id_seq;
+#CREATE SEQUENCE event_id_seq; # for in memory DB
 CREATE TABLE event (
-    id integer NOT NULL DEFAULT nextval('event_id_seq'),
+#    id integer NOT NULL DEFAULT nextval('event_id_seq'), # for in memory DB
+    id integer primary key auto_increment,
     userid integer NOT NULL,
     eventname varchar(255),
     eventtime varchar(255)
@@ -29,10 +32,10 @@ CREATE TABLE event (
 # --- !Downs
 
 DROP TABLE event;
-DROP SEQUENCE event_id_seq;
+#DROP SEQUENCE event_id_seq; # for in memory DB
 
 DROP TABLE symptom;
-DROP SEQUENCE symptom_id_seq;
+#DROP SEQUENCE symptom_id_seq; # for in memory DB
 
 DROP TABLE user;
-DROP SEQUENCE user_id_seq;
+#DROP SEQUENCE user_id_seq; # for in memory DB
