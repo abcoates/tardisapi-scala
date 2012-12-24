6f38768b3e0e5a5ae706acb3ced80e0c712021ed
Anthony B. Coates <abcoates@gmail.com>
2012-12-24 18:11:01 +0000

    Separated out 'Person' class and table from 'Patient'.

---
+++ app/models/Person.scala
@@ -0,0 +1,12 @@
+package models
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tony
+ * Date: 24/12/12
+ * Time: 16:41
+ * To change this template use File | Settings | File Templates.
+ */
+class Person {
+
+}
